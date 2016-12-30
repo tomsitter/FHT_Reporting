8d02b404f68dc3a6e5dffc76324ec49b238f085c
Tom Sitter <thomas.sitter@gmail.com>
2016-12-30 14:40:46 -0500

    Fixes to MH tracking code

--- quarterly_reporting/python/schedule_a.py
+++ quarterly_reporting/python/schedule_a.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from collections import defaultdict, namedtuple
 from operator import attrgetter
 import pdb
+import random
+import string
 
 __author__ = 'Tom Sitter'
 
@@ -31,13 +33,13 @@ def weight_mgmt_change():
             print(patient, ": Only 1 weight")
 
 def weight_mgmt_appt_type():
-
+    
     billing_file = 'WM_AllVisits_20160401-20161231.csv'
     
     visits = build_dict(filename=os.path.join(filepath, billing_file),
                        key=('First Name', 'Last Name'),
                        event_fields=Event(date='Bill Date', value='PCode'))
-
+    
     summary = {
         'only_INI': [],
         'WFU_WGS': [],
@@ -50,57 +52,52 @@ def weight_mgmt_appt_type():
             summary['WFU_WGS'].append(events)
 
 def mental_health(billing_file='MH_Billing_20160401-20160930.csv',
+                  phq9_file = 'MH_PHQ9s_Q1-Q3_2016.csv',
+                  gad7_file = 'MH_GAD7s_Q1-Q3_2016.csv',
                   fields=('Bill Date', 'PCode'),
                   init_code='MHINI',
                   event_filter=filter_events_by_code):
-
-    followup_code = 'MHFU'
-
-    summary = {
-        'no fu': [],
-        'no init': [],
-        'fu': []
-    }
-
-    # phq9_file = 'MH_PHQ9_20160401-20160930.csv'
-    # gad7_file = 'MH_GAD7_20160401-20160930.csv'
-
+        
     bills = build_dict(filename=os.path.join(filepath, billing_file),
                        key='PHN',
                        event_fields=Event(date=fields[0], value=fields[1]))
-
-    # phq9s = build_dict(filename=os.path.join(filepath, phq9_file),
-    #                    key='PHN',
-    #                    event_fields=Event(date='Observation Date', value='Value'))
-
-    # gad7s = build_dict(filename=os.path.join(filepath, gad7_file),
-    #                    key='PHN',
-    #                    event_fields=Event(date='Observation Date', value='Value'))
-
+    
+    phq9s = build_dict(filename=os.path.join(filepath, phq9_file),
+                       key='PHN',
+                       event_fields=Event(date='Observation Date', value='Value'))
+    
+    gad7s = build_dict(filename=os.path.join(filepath, gad7_file),
+                       key='PHN',
+                       event_fields=Event(date='Observation Date', value='Value'))
+    
+    scores = defaultdict(dict)
+    
     for patient, events in bills.items():
+        
         init_date, filtered_events = event_filter(events, init_code=init_code)
-
+        
         if not init_date:
             # print('Patient: ', patient, 'Status: N/A -- No Init Date')
-            # summary['no init'].append(patient)
             continue
-
-        # filtered_phq9_events = filter_events_by_date(phq9s[patient],
-        #                                              init_date=init_date)
-        # filtered_gad7_events = filter_events_by_date(gad7s[patient],
-        #                                              init_date=init_date)
-
-        # if len(filtered_phq9_events) >= 2:
-        #     init_phq9, final_phq9 = filtered_phq9_events[0], filtered_phq9_events[-1]
-        #     print('Patient: ', patient, 'PHQ9 Status: ', get_patient_status(int(init_phq9.value),
-        #                                                                     int(final_phq9.value)))
-
-        # if len(filtered_gad7_events) >= 2:
-        #     init_gad7, final_gad7 = filtered_gad7_events[0], filtered_gad7_events[-1]
-        #     print('Patient: ', patient, 'GAD7 Status: ', get_patient_status(int(init_gad7.value),
-        #                                                                     int(final_gad7.value)))
-
-    return bills
+            
+        filtered_phq9_events = filter_events_by_date(phq9s[patient],
+                                                     init_date=init_date)
+        filtered_gad7_events = filter_events_by_date(gad7s[patient],
+                                                     init_date=init_date)
+        
+        if len(filtered_phq9_events) >= 2:
+            init_phq9, final_phq9 = filtered_phq9_events[0], filtered_phq9_events[-1]
+            # print('Patient: ', patient, 'PHQ9 Status: ', get_patient_status(int(init_phq9.value),
+                                                                            # int(final_phq9.value)))
+            scores[patient]['phq9'] = get_patient_status(int(init_phq9.value), int(final_phq9.value))
+            
+        if len(filtered_gad7_events) >= 2:
+            init_gad7, final_gad7 = filtered_gad7_events[0], filtered_gad7_events[-1]
+            # print('Patient: ', patient, 'GAD7 Status: ', get_patient_status(int(init_gad7.value),
+                                                                            # int(final_gad7.value)))
+            scores[patient]['gad7'] = get_patient_status(int(init_gad7.value), int(final_gad7.value))
+            
+    return scores
 
 
 def build_dict(filename=None, key=None, event_fields=None, encoding='utf-8-sig'):
@@ -112,7 +109,10 @@ def build_dict(filename=None, key=None, event_fields=None, encoding='utf-8-sig')
             if isinstance(key, tuple):
                 patient_key = ' '.join([row[e] for e in key]).title()
             else:
-                patient_key = row[key]
+                if row[key] == '':
+                    patient_key = random_identifier()
+                else:
+                    patient_key = row[key]
             event = Event(date=datetime.strptime(row[event_fields.date], date_format).date(),
                           value=row[event_fields.value])
             events[patient_key].append(event)
@@ -123,19 +123,19 @@ def filter_events_by_code(events, init_code=None):
     """Given a list of events and an initial billing code,
     Return a sorted list of all billing events that occurred from the
     date of the most recent initial billing onward"""
-
+    
     # Sort events by date and get the most recent initial visit date
     sorted_event_dates = sorted([event.date for event in events if event.value == init_code])
-
+    
     if not sorted_event_dates:
         return None, []
     else:
         init_date = sorted_event_dates[-1]
-
+        
     # Make sure patient has had a initial visit billed
     if not init_date:
         return None, []
-
+        
     # Now go through all events and pull values for any dates occurring on or after the initial visit date
     return (init_date, [event
                         for event in sorted(events, key=attrgetter('date'))
@@ -158,3 +158,10 @@ def get_patient_status(start, end):
         return 'Same'
     elif end > start:
         return 'Worsened'
+
+def random_identifier():
+    print('Generating Random identifier')
+    return ''.join(
+        [random.choice(string.digits) for _ in range(10)] +
+        [random.choice(string.ascii_uppercase) for _ in range(2)]
+    )
